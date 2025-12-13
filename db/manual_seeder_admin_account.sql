e07795476fdc833d4999f6f7afd410a5c34497bb
ysk-compile <141384233+yosoykyle@users.noreply.github.com>
2025-12-13 12:08:06 +0800

    admin seeder account

---
+++ db/manual_seeder_admin_account.sql
@@ -0,0 +1,2 @@
+INSERT INTO `users` (`email`, `password_hash`, `first_name`, `last_name`, `role`, `is_archived`)
+    VALUES ('admin@admin.com', '$2y$10$yaUtuRH.TB7lZzLq1.yOme7r37yGj44cCrR/0ChHQMdf2ilE9hiB1q', 'System', 'Admin', 'admin', 0);
